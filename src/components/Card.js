0a1b435d9694a9eecd1eee9cc377e9df76acb890
marinawangmw <marinawangmw@gmail.com>
2020-05-01 18:11:49 -0300

    Initial commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// index importa robots, y se lo pasa a card
+const Card = ({name,email,id}) => {
+	return (
+		<div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
+			<div>
+				<h2>{name}</h2>
+				<p>{email}</p>
+			</div>
+		</div>);
+} 
+
+export default Card;
