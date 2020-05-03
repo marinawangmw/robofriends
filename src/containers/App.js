6effeab84f3c71d190b4a7bfb6f4f3a2835408cd
marinawangmw <marinawangmw@gmail.com>
2020-05-03 15:44:26 -0300

    + AJAX

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,6 +15,7 @@ class App extends Component {
   }
 
   componentDidMount(){
+  	//AJAX
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => { return response.json()})
       .then(users => this.setState({robots:users}));
