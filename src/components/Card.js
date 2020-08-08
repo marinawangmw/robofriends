ec2f70fded8d33eb28692f67eb5e556c4ead5334
marinawangmw <marinawangmw@gmail.com>
2020-08-07 22:23:35 -0300

    changes favicon and tab title

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 // index importa robots, y se lo pasa a card
 const Card = ({name,email,id}) => {
 	return (
-		<div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+		<div className='tc grow bg-light-green br3 pa2 ma2 dib bw2 shadow-5'>
 			<img alt='robots' src={`https://robohash.org/${id}?size=200x200`}/>
 			<div>
 				<h2>{name}</h2>
